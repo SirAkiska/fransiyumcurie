560229fc4664026c1ba14ff0504a7e4796f2ed3f
SirAkiska <104687005+SirAkiska@users.noreply.github.com>
2022-05-14 15:31:47 +0300

    eren periyodik tablo

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,9 +1,9 @@
 from PIL import Image
 print("*****FRANSİYUM CURİE HOŞ GELDİNİZ*****")
 print("element bilgileri için -->1 \n element resimleri için -->2 \n günlük karışımlar için -->3 \n asit-baz için -->4 ")
-a=int(input("Lütfen tercıhte bulununuz"))
+a=int(input("Lütfen tercıhte bulununuz:"))
 if a==1:
-    print("istediğiniz elementin atom numarasını giriniz")
+    print("istediğiniz elementin atom numarasını giriniz:")
     f0=open("D:\GitHub/fransiyumcurie\dosyalar\element.txt","r")
     f1=open("D:\GitHub/fransiyumcurie\dosyalar\sembol.txt","r")
     f2=open("D:\GitHub/fransiyumcurie\dosyalar/tür.txt","r")
@@ -18,7 +18,7 @@ if a==1:
     list5=list(f4)
     list6=list(f5)
     list7=list(f6)
-    x=int(input("1-118 arasında atom numarası giriniz"))
+    x=int(input("1-118 arasında atom numarası giriniz:"))
     x-=1
     c0=list1[x]
     c1=list2[x]
@@ -27,7 +27,7 @@ if a==1:
     c4=list5[x]
     c5=list6[x]
     c6=list7[x]
-    print("verdiğiniz atom numarasınna göre")
+    print("*****verdiğiniz atom numarasınna göre*****")
     print("elementin adı-->%s"% (c0))
     print("elementin bilimsel gösterimi -->%s"%(c1))
     print("elemen türü -->%s"%(c2))
